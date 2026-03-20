f105369ecaa7650c6ef4f9af7bdec8eaecfbd47d
AngadSandhu <angad.sandhu16@gmail.com>
2026-03-20 18:07:24 +0530

    02: Defining flow & adding reqd regex

--- src/messageValidator.ts
+++ src/messageValidator.ts
@@ -38,13 +38,54 @@ export interface MessageOutput {
 
 */
 
+// const BLOCKED_CONTENT: Array<string> = [`tourhq`, `toursbylocals`, `withlocals`, `facebook`, `twitter`, `instagram`, `gmail`, `email`, `skype`, `cash`, `recruiter`];
+// const WHITELISTED_DOMAINS: Array<string> = [`gowithguide.com`, `travelience.co.jp`, `zoom.us`];
+  
+  
+function parseMessage(message: string, bookingStatus: 'unconfirmed' | 'confirmed', sender: 'customer' | 'supplier'): string {
+  let parsedMessage: string = message;
 
+  if(bookingStatus==='unconfirmed' && sender==='supplier'){
+    let phoneRegex: RegExp = /^\+?[0-9\s.\-()]{7,25}$/; 
+    let emailRegex: RegExp = /\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}\b/gi;;
+    let linkRegex: RegExp = /\b(?:https?:\/\/|www\.)?[\w-]+\.[\w-]{2,}(?:\/[\w\-\.\/]*)*\b/;
+
+    let combinedRegex: RegExp = new RegExp(phoneRegex.source + '|' + emailRegex.source + '|' + linkRegex.source, 'gi');
+
+    // check for whitelisted stuff
+    // Check for the match : phone, email, link
+
+    parsedMessage = message.replace(combinedRegex,(match)=>{
+      const isAllowed = match.includes('zoom.us') ||  match.includes('gowithguide.com') || match.includes('travelience.co.jp');
+      if (isAllowed) {
+        return match;
+      } else {
+        return '*'.repeat(match.length);
+      }
+    });
+
+  } 
+  return parsedMessage;
+}
 
 // ToDo: implement business logic from README.md
 export function validateMessage(input: MessageInput): MessageOutput {
+  // let messsage: string = input.message;
+
+  let parsedMessage: string;
+  // let blocked: Array<string> = [];
+
+  if(!input.hasConfirmedBooking){
+    // Block the messages including any contact info
+    
+    parsedMessage = parseMessage(input.message, 'unconfirmed', input.sender);
+  } else {
+    parsedMessage = parseMessage(input.message, 'confirmed', input.sender);
+  }
+
   return {
     sender: input.sender,
-    message: input.message,
+    message: parsedMessage,
     hasConfirmedBooking: input.hasConfirmedBooking,
     blocked: [],
   };
