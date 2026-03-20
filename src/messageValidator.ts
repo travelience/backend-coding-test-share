80e83330a2025e33f33767084a373ea960a6c448
AngadSandhu <angad.sandhu16@gmail.com>
2026-03-20 17:40:17 +0530

    01: Planning Code Flow

--- src/messageValidator.ts
+++ src/messageValidator.ts
@@ -12,6 +12,34 @@ export interface MessageOutput {
   blocked: string[];
 }
 
+/*
+  Code Flow:
+  BOOKING STATUS = false
+  - block/mask the phone number
+  - block email always,only allow email from goWithGuide and travelience
+  - block all links, always allow links from goWithGuide and travelience (both supplier/customer)
+
+  BOOKING STATUS = true
+  - allow the phone number
+  - block email always, only allow email from goWithGuide and travelience
+  - block all links, always allow links from goWithGuide, zoom (Supplier)
+  - Allow all links (Customer)
+
+
+
+
+  BLOCKING RULES
+  - Replace blocked content with `******`
+  - Preserve message structure and spacing
+  - Track what was blocked in the `blocked` array
+
+  BLOCKED/RESTRICTED CONTENT
+  - Keywords: `tourhq`, `toursbylocals`, `withlocals`, `facebook`, `twitter`, `instagram`, `gmail`, `email`, `skype`, `cash`, `recruiter`
+
+*/
+
+
+
 // ToDo: implement business logic from README.md
 export function validateMessage(input: MessageInput): MessageOutput {
   return {
